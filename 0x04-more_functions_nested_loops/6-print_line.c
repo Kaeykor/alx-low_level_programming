ca8f2bc427de2a2ae8c512b817bbcbb3e3ffac44
Kaeykor <onyekachukwuakor2002@gmail.com>
2022-09-15 16:59:55 -0700

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ *
+ * Description: Prints lines
+ *@n: number of lines
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int line_num;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (line_num = 1; line_num <= n; line_num++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
